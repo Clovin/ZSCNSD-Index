d8a4dfaee8c180a739397b32cea8d7fefaeac481
LunziQwQ <me@lunzi.space>
2016-07-14 22:45:42 +0800

    成功添加滚动一格功能，然而仍有一点小bug，在最后两个标签卡的切换过程中，active类的改变有些问题，暂未发现具体原因

--- js/js.js
+++ js/js.js
@@ -85,8 +85,8 @@
     //  点击nav事件
     function click_nav() {
         //参数为JQ元素，想要模拟点击对应的a标签元素，多加了一个参数试图让函数更灵活，滚动页面时模拟点击标签
-        // var target =(arguments[1]) ? arguments[1].attr("href") : $($(event.target).attr("href"));
-        var target = $($(event.target).attr("href"));
+        var target =(arguments[0].length) ? $(arguments[0].attr("href")) : $($(event.target).attr("href"));
+        // var target = $($(event.target).attr("href"));
         var current = document.body.scrollTop;
         if(target!=='#main_content'){
             var temp = target.offset().top-230;
@@ -108,19 +108,17 @@
         var endCurrent = document.body.scrollTop;
         if (startCurrent.position == get_position()) {  //当滚动完position未改变时
             if(endCurrent > startCurrent.scroll){
-                console.log("down");
                 //向下滚动，模拟点击下一个标签
                 if($($('a.active').parent().next('li')))
-                    $('a.active').parent().next('li').children('a').click();
+                    click_nav($('a.active').parent().next('li').children('a'));
             }else{
-                console.log("up");
                 //向上滚动，模拟点击上一个标签
                 if($($('a.active').parent().prev('li')))
-                    $('a.active').parent().prev('li').children('a').click();
+                    click_nav($('a.active').parent().prev('li').children('a'));
             }
         }else{
             //模拟点击对应position的标签
-            $('a.active').click();
+            click_nav($('a.active'));
         }
     }
 
